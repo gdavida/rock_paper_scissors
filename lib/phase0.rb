3ca5e1efb59133effc8275b854c8034708cb474b
Davida Gaffney <gdavida@gmail.com>
2016-03-24 11:11:41 -0500

    tie or player 1 rock choices

---
+++ lib/phase0.rb
@@ -0,0 +1,14 @@
+
+
+#if their choices are the same, its a tie!
+if player_1_choice == player_2_choice
+	return "TIE!"
+#if player one chooses rock,
+#first check scenario where player 1 would win,
+#or else we can assume player 2 would win.
+elsif player_1_choice == "rock"
+	if player_2_choice == "scissors"
+		return "PLAYER 1 WINS!"
+	else
+		return "PLAYER 2 WINS"
+	end
