518c8acf672791456894359cdd10bec4e1db79ed
Davida Gaffney <gdavida@gmail.com>
2016-03-24 11:38:29 -0500

    Change accepting words to accepting number for 1- rock, 2-paper, 3-scissors

--- lib/phase0.rb
+++ lib/phase0.rb
@@ -1,13 +1,17 @@
 require "pry"
+##	1	 	rock
+##	2 	paper
+##	3 	scissors
 
 puts "-------------------------------------------------"
 puts "-------------------------------------------------"
 puts "LET'S PLAY ROCK, PAPER, SCISSORS!\n"
-print "\nPLAYER 1, please choose:\nrock\npaper\nscissors\n\n"
-player_1_choice = gets.chomp.downcase!
+print "\nPLAYER 1, please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+player_1_choice = gets.chomp
+
 puts "-------------------------------------------------"
-print "\nPLAYER 2, please choose:\nrock\npaper\nscissors\n\n"
-player_2_choice = gets.chomp.downcase!
+print "\nPLAYER 2, please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+player_2_choice = gets.chomp
 
 puts "-------------------------------------------------\n\n"
 
@@ -17,8 +21,8 @@ if player_1_choice == player_2_choice
 #if player one chooses rock,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
-elsif player_1_choice == "rock"
-	if player_2_choice == "scissors"
+elsif player_1_choice == "1" #rock
+	if player_2_choice == "2" #scissors
 		print "PLAYER 1 WINS!!"
 	else
 		print "PLAYER 2 WINS!!"
@@ -26,8 +30,8 @@ elsif player_1_choice == "rock"
 #if player one chooses paper,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
-elsif player_1_choice == "paper"
-	if player_2_choice == "rock"
+elsif player_1_choice == "2" #paper
+	if player_2_choice == "1"	#rock
 		print "PLAYER 1 WINS!!"
 	else
 		print "PLAYER 2 WINS!!"
@@ -35,8 +39,8 @@ elsif player_1_choice == "paper"
 	#if player one chooses scissors,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
-elsif player_1_choice == "scissors"
-	if player_2_choice == "paper"
+elsif player_1_choice == "3" #scissors
+	if player_2_choice == "2" #paper
 		print "PLAYER 1 WINS!!"
 	else
 		print "PLAYER 2 WINS!!"
