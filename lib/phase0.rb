e5815d98662b6d941903ccf378af65d10b9936f5
Davida Gaffney <gdavida@gmail.com>
2016-03-24 11:23:16 -0500

    Add downcase! for answers to accept capitals

--- lib/phase0.rb
+++ lib/phase0.rb
@@ -1,42 +1,44 @@
 require "pry"
 
-puts "LET'S PLAY ROCK, PAPER, SCISSORS!"
-print "PLAYER 1, please choose:\nrock\npaper\nscissors\n\n"
-player_1_choice=gets.chomp
 puts "-------------------------------------------------"
-print "PLAYER 2, please choose:\nrock\npaper\nscissors"
-player_2_choice=gets.chomp
+puts "-------------------------------------------------"
+puts "LET'S PLAY ROCK, PAPER, SCISSORS!\n"
+print "\nPLAYER 1, please choose:\nrock\npaper\nscissors\n\n"
+player_1_choice = gets.chomp.downcase!
+puts "-------------------------------------------------"
+print "\nPLAYER 2, please choose:\nrock\npaper\nscissors\n\n"
+player_2_choice = gets.chomp.downcase!
 
 puts "-------------------------------------------------\n\n"
 
 #if their choices are the same, its a tie!
 if player_1_choice == player_2_choice
-	return "TIE!"
+	print "TIE!"
 #if player one chooses rock,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
 elsif player_1_choice == "rock"
 	if player_2_choice == "scissors"
-		return "PLAYER 1 WINS!"
+		print "PLAYER 1 WINS!!"
 	else
-		return "PLAYER 2 WINS"
+		print "PLAYER 2 WINS!!"
 	end
 #if player one chooses paper,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
 elsif player_1_choice == "paper"
 	if player_2_choice == "rock"
-		return "PLAYER 1 WINS!"
+		print "PLAYER 1 WINS!!"
 	else
-		return "PLAYER 2 WINS"
+		print "PLAYER 2 WINS!!"
 	end
 	#if player one chooses scissors,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
 elsif player_1_choice == "scissors"
 	if player_2_choice == "paper"
-		return "PLAYER 1 WINS!"
+		print "PLAYER 1 WINS!!"
 	else
-		return "PLAYER 2 WINS"
+		print "PLAYER 2 WINS!!"
 	end
 end
