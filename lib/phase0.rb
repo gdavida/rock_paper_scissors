ca2bf5a297d2a87ba8dec8e2c9d271d40af7d331
Davida Gaffney <gdavida@gmail.com>
2016-03-24 12:02:38 -0500

    Validate the number chosen was either 1,2, or 3

--- lib/phase0.rb
+++ lib/phase0.rb
@@ -8,10 +8,18 @@ puts "-------------------------------------------------"
 puts "LET'S PLAY ROCK, PAPER, SCISSORS!\n"
 print "\nPLAYER 1, please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
 player_1_choice = gets.chomp
+while (player_1_choice != "1") && (player_1_choice != "2") && (player_1_choice != "3")
+	print "Not valid, enter a valid number. Please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+	player_1_choice = gets.chomp
+end
 
 puts "-------------------------------------------------"
 print "\nPLAYER 2, please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
 player_2_choice = gets.chomp
+while (player_2_choice != "1") && (player_2_choice != "2") && (player_2_choice != "3")
+	print "Not valid, enter a valid number. Please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+	player_1_choice = gets.chomp
+end
 
 puts "-------------------------------------------------\n\n"
 
@@ -23,26 +31,26 @@ if player_1_choice == player_2_choice
 #or else we can assume player 2 would win.
 elsif player_1_choice == "1" #rock
 	if player_2_choice == "3" #scissors
-		print "PLAYER 1 WINS!!\n rock crushes scissors."
+		print "PLAYER 1 WINS!!\n Rock crushes Scissors."
 	else 
-		print "PLAYER 2 WINS!!\n paper covers rock"
+		print "PLAYER 2 WINS!!\n Paper covers Rock"
 	end
 #if player one chooses paper,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
 elsif player_1_choice == "2" #paper
 	if player_2_choice == "1"	#rock
-		print "PLAYER 1 WINS!!\n paper covers rock"
+		print "PLAYER 1 WINS!!\n Paper covers Rock"
 	else
-		print "PLAYER 2 WINS!!\n scissors cuts paper"
+		print "PLAYER 2 WINS!!\n Scissors cuts Paper"
 	end
 	#if player one chooses scissors,
 #first check scenario where player 1 would win,
 #or else we can assume player 2 would win.
 elsif player_1_choice == "3" #scissors
 	if player_2_choice == "2" #paper
-		print "PLAYER 1 WINS!!\n scissors cuts paper"
+		print "PLAYER 1 WINS!!\n Scissors cuts Paper"
 	else
-		print "PLAYER 2 WINS!!\n rock crushes paper"
+		print "PLAYER 2 WINS!!\n Rock crushes Paper"
 	end
 end
