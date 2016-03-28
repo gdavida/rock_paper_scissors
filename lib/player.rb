093471f7bd208c11033f9e91bc1502676a4b8c1e
Davida Gaffney <gdavida@gmail.com>
2016-03-28 08:34:42 -0500

    Monday push

--- lib/player.rb
+++ lib/player.rb
@@ -5,10 +5,45 @@ class Player
 attr_accessor :name, :moves
 
 def initialize(options={})
-	@name			=	options[:name]
-	@moves		=	options[:moves]
-	@wins			= options[:wins]
-	@ties			= options[:ties]
-	@loses		= options[:loses]
+	@name				=	options[:name]
+	@moves			=	options[:moves]
+	@total_wins	= options[:total_wins] #should store the number
+	@score			= options[:score] # should always start at 0 with each new game
 end
+#this hash shows what that key needs to be up against in order for the key to be the winner
+how_to_win_if_you_choose = ["rock": "scissors", "paper": "rock", "scissors": "paper"]
+
+
+
+def validate_rps_choice(choice)
+	while (choice != "1") && (choice != "2") && (choice != "3")
+		print "Not valid, enter a valid number. Please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+		choice = gets.chomp
+	end
+	choice
+end 
+
+
+
+
+
+
+def lets_see_who_wins(player_1_choice, player_2_choice)
+	if player_1_choice == player_2_choice
+		"TIE"
+		# this looks up p1's choice to see what that p1 choice needs to be up against in order to be the winner, and if player 2's choice is the same as what p1 needs to be against to win, then of course p1 wins
+	elsif player_2_choice == how_to_win_if_you_choose[player_1_choice]
+		"PLAYER 1 WINS!!!"
+		Player1.total_wins += 1
+		Player1.score += 1
+		#otherwise we can assume that if there is no tie and p1 doesnt win, then p2 does
+	else "PLAYER 2 WINS!!!"
+		Player2.total_wins += 1
+		Player2.score += 1
+	end
+	puts "Player 1: #{Player(1).score}"
+	puts "Player 2: #{Player(2).score}"
+end
+
+
 
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require "pry"
 require_relative "lib/player.rb"
 
 # 
@@ -10,26 +11,85 @@ require_relative "lib/player.rb"
 
 #------------------------------------
 
+#START A GAME
+
+def start_a_game
+	puts "Player 1"
+	#maybe instead of this just automatically show a list of names that have already played with an additional option being, add my name ... maybe not, idk
+	#but these two processes are identical except for the Player 1 vs Player 2, so maybe add the player # to the Player class then just refer to that?
+	#player 1
+	print "Are you an existing player? (y/n)"
+	choice = gets.chomp
+	if choice == "n*"
+		add_a_player
+	elsif choice == "y*"
+		puts Player.name
+		#look at Kyle's code and find how he got the list of the names for guitar players and choosing one
+	end
+	while choice != "y*" 
+		print "Not a valid repsonse, are you an existing player? (y/n)"
+		choice = gets.chomp
+	end
+	puts "Player 1 is #{Player.name}"
+
+
+	puts "\n--------------------------"
+
+
+	#player 2
+	print "\nPlayer 2, are you an existing player? (y/n)"
+	choice = gets.chomp
+	if choice == "n*"
+		add_a_player
+	elsif choice == "y*"
+		puts Player.name
+		#look at Kyle's code and find how he got the list of the names for guitar players and choosing one
+	end
+	while choice != "y*" 
+		print "Not a valid repsonse, are you an existing player? (y/n)"
+		choice = gets.chompend
+	puts "Player 2 is #{Player.name}"
+	puts "\n--------------------------"
+	play_a_game # now that you've got the game set up with the players chosen, its time to play the game
+	end
+
+#------------------------------------
+
+#ADD A PLAYER
+#this is def part of the driver
+def add_a_new_player
+  puts "\n\nADD A NEW PLAYER\n\n"
+  print "What is your name? "
+  name = gets.chomp
+
+  #need default value for illustrator to be "none", in case there is none
+  Player.new(name: name)
+
+end
+
+
+#------------------------------------
+
 #PLAY A GAME
+#Procedures
+# eventually need to make the "steps" its own method so we are just calling player 1 (and maybe the play 1 vs player 2 is something indicated in this player's array)
+def  play_a_game
+	#PLAYER 1 choses a valid selection of r,p,s
+	print "\nPLAYER 1, please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+	choice = gets.chomp
+	validate_rps_choice(choice)
+	player_1_choice = choice
 
+	puts "-------------------------------------------------"
+	print "\nPLAYER 2, please select the corresponding number for your choice:\n1  rock\n2  paper\n3  scissors\n\n"
+		#PLAYER 2 choses a valid selection of r,p,s
+	choice = gets.chomp
+	validate_rps_choice(choice)
+	player_2_choice = choice
 
+	puts lets_see_who_wins(player_1_choice, player_2_choice)
 
-
-#------------------------------------
-
-
-
-
-
-
-
-#------------------------------------
-
-
-
-
-
-
+end
 
 #------------------------------------
 
